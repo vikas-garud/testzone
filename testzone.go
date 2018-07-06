d6351cba770823826b08341a234a8cec94428571
Vikas Garud <garud@sea-ml-00001238.olympus.f5net.com>
2018-07-06 13:47:21 -0700

    added main function

---
+++ testzone.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// all trials on the repo will be done here on this repo
+
+	fmt.Printf("All trials on git repo will be done here.....\n")
+	fmt.Printf("line one .....\n")
+	fmt.Printf("line two .....\n")
+	fmt.Printf("line three .....\n")
+	fmt.Printf("line four .....\n")
+	fmt.Printf("line five .....\n")
+}
